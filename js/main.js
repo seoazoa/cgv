ebe1de827495586bef89eaaff3363fe915c54f8e
seoa <tyg04336@gmail.com>
2024-01-16 18:12:15 +0900

    이벤트 완료

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    var MovieSwiper = new Swiper(".MovieSwiper" , {
+        direction: "horizontal",
+        Loop:true,
+        slidesPerView: 5,
+        spaceBetween: 30,
+        // navigation
+        navigation: {
+            nextEl: ".next",
+            prevEl: ".prev",
+        },
+        breakpoints:{
+            // 320 이상일때
+            320:{
+                slidesPerView:1,
+            },
+            // 480 이상일때
+            480:{
+                slidesPerView:2,
+                spaceBetween: 30,
+            },
+            // 768 이상일때
+            768:{
+                slidesPerView:5,
+                // spaceBetween: 30,
+            },
+        }
+    })
+    })
